c63a13ab0ad57b0c54ebdad3cf0c2b5c5b9c4603
Felipe Melo <felipemelozx@gmail.com>
2024-12-19 15:14:26 -0300

    feat: first commit

---
+++ src/main/java/com/felipemelozx/onebitflix/OneBitFlixApplication.java
@@ -0,0 +1,13 @@
+package com.felipemelozx.onebitflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneBitFlixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneBitFlixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipemelozx/onebitflix/OneBitFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipemelozx.onebitflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneBitFlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
