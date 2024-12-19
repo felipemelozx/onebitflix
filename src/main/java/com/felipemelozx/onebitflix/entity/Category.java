b484d8a07bb15e23ff1e4af9f207c54c346e1b6c
Felipe Melo <felipemelozx@gmail.com>
2024-12-19 15:40:57 -0300

    feat: Create Category class  with JPA mapping for table in database

---
+++ src/main/java/com/felipemelozx/onebitflix/entity/Category.java
@@ -0,0 +1,80 @@
+package com.felipemelozx.onebitflix.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+public class Category {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Integer id;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false)
+  private Integer position;
+
+  @Column(
+      name = "created_at",
+      nullable = false)
+  private Date createdAt;
+
+  @Column(
+      name = "updated_at",
+      nullable = false)
+  private Date updateAt;
+
+  public Category(Integer id, String name, Integer position, Date createdAt, Date updateAt) {
+    this.id = id;
+    this.name = name;
+    this.position = position;
+    this.createdAt = createdAt;
+    this.updateAt = updateAt;
+  }
+
+  public Category() {
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Integer getPosition() {
+    return position;
+  }
+
+  public void setPosition(Integer position) {
+    this.position = position;
+  }
+
+  public Date getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(Date createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public Date getUpdateAt() {
+    return updateAt;
+  }
+
+  public void setUpdateAt(Date updateAt) {
+    this.updateAt = updateAt;
+  }
+}
